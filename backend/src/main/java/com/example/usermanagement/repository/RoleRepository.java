e2076a7f130d5b19563eed67f6a041cd10203f17
Nguyen Van Nga <ngaiuai1202@gmail.com>
2025-12-26 11:51:21 +0700

    lần đầu

---
+++ backend/src/main/java/com/example/usermanagement/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.usermanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.usermanagement.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
